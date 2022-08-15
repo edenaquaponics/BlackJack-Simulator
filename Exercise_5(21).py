34b58a947cfbeb86d9cce893e944dba122d3a373
Mark Mwanga <markvictormwanga@gmail.com>
2022-08-15 14:47:19 +0300

    Unnested functions from Blackjack class.

--- Exercise_5(21).py
+++ Exercise_5(21).py
@@ -5,50 +5,48 @@ Created on Sun Aug 14 21:19:54 2022
 @author: LENOVO
 """
 
-class Blackjack:
-
-    def __init__(self) -> None:
-        pass
-
-    def player_turn() -> int:
-        pass
-
-    def dealers_turn() -> int:
-        pass
-
-    def start():
-        pass
+def init_game():
+    pass
 
 
-def playing_game():
+def start_game():
+    pass
 
-    import random as rd
 
-    playing_game = 0
-    deck = []
-    #generate the cards
-    def generate_cards(deck):
-        current_card = 1
-        while current_card < 11:
-            i = 0
-            current_card += 1
-            if current_card == 10:
-                for i in range(16):
-                    deck.append(current_card)
-            else:
-                for i in range(4):
-                    deck.append(current_card)
-                    print(current_card)
-        rd.shuffle(deck)             
+def players_turn() -> int:
+    pass
+
+
+def dealers_turn() -> int:
+    pass
+
+
+
+
+# def playing_game():
+
+#     import random as rd
+
+#     playing_game = 0
+#     deck = []
+#     #generate the cards
+#     def generate_cards(deck):
+#         current_card = 1
+#         while current_card < 11:
+#             i = 0
+#             current_card += 1
+#             if current_card == 10:
+#                 for i in range(16):
+#                     deck.append(current_card)
+#             else:
+#                 for i in range(4):
+#                     deck.append(current_card)
+#                     print(current_card)
+#         rd.shuffle(deck)             
     
-    generate_cards(deck)            
-    print(deck)        
-    while playing_game < 1:
-        #the player should draw a card
-        player_deck = []
-        dealer_deck = []
-        
-      
-          
-    
-            
+#     generate_cards(deck)            
+#     print(deck)        
+#     while playing_game < 1:
+#         #the player should draw a card
+#         player_deck = []
+#         dealer_deck = []
