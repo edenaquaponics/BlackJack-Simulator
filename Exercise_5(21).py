6901bcc3292ea7e90077e9bd6d88db5e6bf6a51a
LeonKim <romoleon9@gmail.com>
2022-08-15 20:56:29 +0930

    Create functon to generate deck of cards

---
+++ Exercise_5(21).py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 21:19:54 2022
+
+@author: LENOVO
+"""
+def playing_game():
+
+    import random as rd
+
+    playing_game = 0
+    deck = []
+    #generate the cards
+    def generate_cards(deck):
+        current_card = 1
+        while current_card < 11:
+            i = 0
+            current_card += 1
+            if current_card == 10:
+                for i in range(16):
+                    deck.append(current_card)
+            else:
+                for i in range(4):
+                    deck.append(current_card)
+                    print(current_card)
+        rd.shuffle(deck)             
+    
+    generate_cards(deck)            
+    print(deck)        
+    while playing_game < 1:
+        #the player should draw a card
+        player_deck = []
+        dealer_deck = []
+        
+      
+          
+    
+            
